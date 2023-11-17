bc8c1147198fba3b9cde6b1a78d5434799202881
Sunebari Jack <44901635+sunejay@users.noreply.github.com>
2023-11-17 16:59:07 +0100

    Filter button refactored

--- components/items/FilterButton.jsx
+++ components/items/FilterButton.jsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 
 
-const FilterButton = ({text, url, bg="bg-white", icon=false, img="", imgLight=""}) => {
+const FilterButton = ({text, url, onClick, bg="bg-white", icon=false, img="", imgLight=""}) => {
 
     // let mobPad = isMobile ? "px-5 py-2" : "px-20 py-3";
     const [isHover, setIsHover] = useState(false);
@@ -20,6 +20,7 @@ const FilterButton = ({text, url, bg="bg-white", icon=false, img="", imgLight=""
                 href={url} 
                 onMouseEnter={() => setIsHover(true)}
                 onMouseLeave={() => setIsHover(false)}
+                onClick={onClick}
                 className={`
                     flex py-2 px-4 justify-center items-center font-semibold ${bg} 
                     text-xs font-medium rounded-3xl ${icon && 'gap-1'} tz-border-gray-1 
